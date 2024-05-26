86f0a942df375accf3256c788fe9ca1b00460790
Hossein Mansouri <hsnmsri82@gmail.com>
2024-05-26 18:45:38 +0330

    create config file

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// App Config
+$config = [
+    "uri_path" => "sub",
+    "xray_uri_path" => "subc",
+    "xray_panel_domain" => "0.0.0.0",
+    "xray_panel_port" => "2053",
+    "xray_panel_allow_ssl" => false,
+];
